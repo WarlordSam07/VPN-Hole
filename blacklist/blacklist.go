f9c7b2acda92aa1f75949b88f4dbfb7035c3952b
Pratik Lagaskar <pratik.lagaskar@mitaoe.ac.in>
2022-07-10 17:29:27 +0000

    Update blacklist/blacklist.go

--- blacklist/blacklist.go
+++ blacklist/blacklist.go
@@ -1,1 +1,29 @@
+package main
 
+import (
+	"strings"
+
+	"github.com/miekg/dns"
+)
+
+var privBlacklist = blacklist.New(httpClient)
+
+func isBlacklisted(req *dns.Msg) bool {
+	if req.Opcode != dns.OpcodeQuery {
+		return false
+	}
+
+	if len(req.Question) != 1 {
+		return false
+	}
+
+	q := req.Question[0]
+
+	switch q.Qtype {
+	case dns.TypeAAAA:
+	default:
+		return false
+	}
+
+	return privBlacklist.Contains(strings.TrimRight(q.Name, "."))
+}
